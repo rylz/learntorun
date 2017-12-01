4cfd33f6bb6030289d1c4105918d271be1c5c4f9
Riley Patterson <rpatt@caltech.edu>
2017-12-01 01:28:04 +0000

    ppo2: using mlp policy, tensorboard logging, and velocity reward function
    The tensorboard portion of this could be factored out and reused for other models.
    
    It's also using some weights from qingzhou's ppo2 modifications

--- baselines/ppo2/run_osim.py
+++ baselines/ppo2/run_osim.py
@@ -1,23 +1,36 @@
 #!/usr/bin/env python
-import gym, logging
+import gym
+import logging
+import multiprocessing
+import os.path as osp
+import socket
+import subprocess
+import tensorflow as tf
+
 from baselines import bench, logger
 from baselines.common import set_global_seeds
 from baselines.common.vec_env.subproc_vec_env import SubprocVecEnv
+from baselines.common.vec_env.vec_frame_stack import VecFrameStack
 from baselines.ppo2 import ppo2
-from baselines.ppo2.policies import CnnPolicy, LstmPolicy, LnLstmPolicy
-import os.path as osp
-import tensorflow as tf
-from osim.env import RunEnv
+from baselines.ppo2.policies import CnnPolicy, LstmPolicy, LnLstmPolicy, MlpPolicy
 
-def train(ncpu, num_timesteps, seed, policy):
+from env.velocity_reward import VelocityRewardEnv
+
+POLICIES = {
+    'cnn': CnnPolicy,
+    'lstm': LstmPolicy,
+    'lnlstm': LnLstmPolicy,
+    'mlp': MlpPolicy,
+}
+def train(num_timesteps, seed, policy, ncpu=None, load=None):
     """Train the osim walker environment using an optimized PPO policy.
 
-    policy can be one of the following strings:
-      - 'cnn'
-      - 'lstm'
-      - 'lnlstm'
+    policy can be any of the keys in the above POLICIES dict.
+
+    load is a filename of saved weights, or None.
 
     """
+    ncpu = ncpu or multiprocessing.cpu_count() # default to using all cores
     config = tf.ConfigProto(allow_soft_placement=True,
                             intra_op_parallelism_threads=ncpu,
                             inter_op_parallelism_threads=ncpu)
@@ -27,34 +40,63 @@ def train(ncpu, num_timesteps, seed, policy):
 
     def make_env(rank):
         def env_fn():
-            env = RunEnv(visualize=False)
-            env.seed(seed + rank)
+            env = VelocityRewardEnv(visualize=False)
+            env.seed(seed + 1000 * rank)
             env = bench.Monitor(env, logger.get_dir() and osp.join(logger.get_dir(), str(rank)))
             return env
         return env_fn
-    nenvs = 8
+    nenvs = 2*ncpu
     env = SubprocVecEnv([make_env(i) for i in range(nenvs)])
-    env = make_env(0)()
     set_global_seeds(seed)
     env = VecFrameStack(env, 4)
-    policy = {'cnn' : CnnPolicy, 'lstm' : LstmPolicy, 'lnlstm' : LnLstmPolicy}[policy]
-    ppo2.learn(policy=policy, env=env, nsteps=128, nminibatches=4,
-        lam=0.95, gamma=0.99, noptepochs=4, log_interval=1,
-        ent_coef=.01,
-        lr=lambda f : f * 2.5e-4,
-        cliprange=lambda f : f * 0.1,
-        total_timesteps=int(num_timesteps * 1.1))
+    policy = POLICIES[policy]
+    ppo2.learn(policy=policy, env=env, nsteps=2048, nminibatches=32,
+               lam=0.95, gamma=0.99, noptepochs=10, log_interval=1,
+               ent_coef=0.0,
+               lr=3e-4,
+               cliprange=0.2,
+               total_timesteps=num_timesteps,
+               save_interval=5, load_path=load)
+
+LOG_BASE_PATH = '/home/ubuntu/learntorun/results'
+PORT_BASE = 8080
 
 def main():
     import argparse
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--cpus', help='Number of CPUs', type=int, default=1)
     parser.add_argument('--seed', help='RNG seed', type=int, default=0)
-    parser.add_argument('--policy', help='Policy architecture', choices=['cnn', 'lstm', 'lnlstm'], default='cnn')
+    parser.add_argument('--policy', help='Policy architecture', choices=POLICIES.keys(), default='mlp')
     parser.add_argument('--num-timesteps', type=int, default=int(1e6))
+    parser.add_argument('--load', type=str, default=None)
+    parser.add_argument('--tb', help='Enable Tensorboard', action='store_true')
+    parser.add_argument('--tag', help='Tag for this run in logs', type=str, default=None)
     args = parser.parse_args()
-    logger.configure()
-    train(args.cpus, num_timesteps=args.num_timesteps, seed=args.seed, policy=args.policy)
+
+    # possibly start tensorboard
+    if args.tb:
+        # find the logger dir, possibly as a function of the tag
+        logger.LOG_OUTPUT_FORMATS.append('tensorboard')
+        dir = osp.join(LOG_BASE_PATH, args.tag) if args.tag else None
+        logger.configure(dir=dir)
+        dir = osp.join(dir or logger.get_dir(), 'tb')
+
+        # find an open port. note there's a race between this loop and starting tensorboard
+        # also note we don't support more than 100 tensorboards running at a time (lol)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        for port in range(PORT_BASE, PORT_BASE+100):
+            try:
+                s.bind(("127.0.0.1", port))
+            except socket.error as e:
+                # the port is likely already in use; try the next one
+                continue
+            s.close()
+            break
+
+        # start the tensorboard process in the background
+        subprocess.Popen(["tensorboard", f"--logdir={dir}", f"--port={port}"])
+
+    train(args.num_timesteps, args.seed, args.policy, ncpu=args.cpus, load=args.load)
 
 
 if __name__ == '__main__':
