3bd8acb387ef6de4e4f3ead844fe57eb4baea187
Riley Patterson <rpatt@caltech.edu>
2017-12-04 20:53:50 +0000

    knee_reward: ligament damage added back
    courtesy @maxisawesome

--- env/knee_reward.py
+++ env/knee_reward.py
@@ -13,5 +13,10 @@ class KneeRewardEnv(AugRunEnv):
     knee_l_angle = self.current_state[self.L_KNEE_ANGLE]
     pelvis_x = self.current_state[self.PELVIS]
     head_x = self.current_state[self.HEAD]
-    return .5*velocity - .1*knee_r_angle - .1*knee_l_angle + .5*(head_x - pelvis_x)
+    lig_pen = 0
+    for j in range(20, 26):
+      lig = opensim.CoordinateLimitForce.safeDownCast(self.osim_model.forceSet.get(j))
+      lig_pen += lig.calcLimitForce(self.osim_model.state) ** 2
+
+    return velocity - .2*knee_r_angle - .2*knee_l_angle + .75*(head_x - pelvis_x) - (math.sqrt(lig_pen) * 10e-8)
 
